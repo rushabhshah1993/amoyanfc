b9c0132a923ce9e0fd8a2697609d2a36d5b2f0af
Rushabh Mulraj Shah <rushabhmshah@gofynd.com>
2024-12-23 15:32:56 +0530

    Updating global rank resolver to use utility catch async error function

--- server/resolvers/global-rank.resolver.js
+++ server/resolvers/global-rank.resolver.js
@@ -1,9 +1,12 @@
 /* Model imports */
 import { GlobalRank } from "../models/global-rank.model.js";
+import { Fighter } from "../models/fighter.model.js";
 
 /* Error imports */
 import { NotFoundError } from "../error.js";
-import { Fighter } from "../models/fighter.model.js";
+
+/* Utility imports */
+import { catchAsyncErrors } from "../utils.js";
 
 const globalRankResolver = {
     Query: {
@@ -11,21 +14,21 @@ const globalRankResolver = {
          * Fetches a list of all the global ranks available in the system, specific only for IFL
          * @returns {Promise<Array.<Object>>} - An array of objects where every object is a list of global rank at a certain point in time.
          */
-        getAllGlobalRanks: async() => {
+        getAllGlobalRanks: catchAsyncErrors(async() => {
             const allGlobalRanks = await GlobalRank.find({});
             if(!allGlobalRanks) throw new NotFoundError("Not able to retrieve global ranks");
             return allGlobalRanks;
-        },
+        }),
 
         /**
          * Fetch the latest list of global ranking of fighters
          * @returns {Promise<Object>} - A list of the latest ranking of fighters
          */
-        getCurrentGlobalRank: async() => {
+        getCurrentGlobalRank: catchAsyncErrors(async() => {
             const currentGlobalRank = await GlobalRank.find({isCurrent: true});
             if(!currentGlobalRank) throw new NotFoundError("Not able to retrieve current global rank list");
             return currentGlobalRank;
-        },
+        }),
 
         /**
          * Fetch a filtered list of global ranks
@@ -33,11 +36,11 @@ const globalRankResolver = {
          * @param {Object} args  - Arguments passed to this query
          * @returns {Promise<Array.<Object>} - An array of objects where every object represents a list of global ranking at a certian point in time
          */
-        filterGlobalRanks: async(_, args) => {
+        filterGlobalRanks: catchAsyncErrors(async(_, args) => {
             const filteredList = await GlobalRank.find(args);
             if(!filteredList.length) throw new NotFoundError("Not able to retrieve global ranks");
             return filteredList;
-        }
+        })
     },
     Mutation: {
         /**
@@ -46,10 +49,10 @@ const globalRankResolver = {
          * @param {Object} input - Ranking data passed to this mutation
          * @returns {Promise<Object>} The updated object of the latest rankings added to the system
          */
-        addNewGlobalRankList: async(_, input) => {
+        addNewGlobalRankList: catchAsyncErrors(async(_, input) => {
             const newList = new GlobalRank(...input);
             return await newList.save();
-        },
+        }),
 
         /**
          * Update an existing entry of the global rankings in the system
@@ -59,7 +62,7 @@ const globalRankResolver = {
          * @param {Object} args.input - Data to be updated for a given entry
          * @returns {Promise<Object>} - Object containing the updated data
          */
-        updateGlobalRankList: async(_, {id, input}) => {
+        updateGlobalRankList: catchAsyncErrors(async(_, {id, input}) => {
             const updatedList = await GlobalRank.findByIdAndUpdate(
                 id,
                 {...input},
@@ -67,7 +70,7 @@ const globalRankResolver = {
             );
             if(!updatedList) throw new NotFoundError("List not found");
             return updatedList;
-        },
+        }),
 
         /**
          * Deletes an existing entry 
@@ -76,14 +79,14 @@ const globalRankResolver = {
          * @param {String} args.id - Unique ID of the list that needs to be deleted
          * @returns {String} An updated message confirming if the deletion happened successfully
          */
-        deleteGlobalRankList: async(_, { id }) => {
+        deleteGlobalRankList: catchAsyncErrors(async(_, { id }) => {
             const deletedList = await GlobalRank.findByIdAndDelete(id);
             if(!deletedList) throw new NotFoundError("List not found");
             return "Succssfully deleted the list";
-        }
+        })
     },
     Fighter: {
-        fighters: async(parent) => {
+        fighters: catchAsyncErrors(async(parent) => {
             const enrichedFighters = parent.fighters.map(async(fighter) => {
                 const fighterDetails = await Fighter.findById(fighter.fighterId);
                 return {
@@ -92,7 +95,7 @@ const globalRankResolver = {
                 }
             });
             return enrichedFighters;
-        }
+        })
     }
 }
 
